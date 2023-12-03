21cfe556f8be1f9e48b232b7c70b82e8f7d2f4a3
osmn-byhn <osmanbeyhan12@gmail.com>
2023-12-03 20:49:50 +0300

    commit for started react and edited server/index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io")
+
+const server = http.createServer(app);
+
+app.use(cors())
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected: ", socket.id);
+    })
+})
+
+
+server.listen(3001, () => {
+    console.log("SERVER RUNNING ON: http//localhost:3001");
+})
